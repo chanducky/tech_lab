d4b64384a5fbfa8eaa83cfa92f173d7acf73d51f
Chandra <chandu.cky@gmail.com>
2017-09-09 18:17:13 +0530

    Disjoint set -Union By Rank and Path Compression

--- codelab/src/graph/DisjointSetUnionAndPathCompression.java
+++ codelab/src/graph/DisjointSetUnionAndPathCompression.java
@@ -11,18 +11,20 @@ import java.util.Set;
  */
 
 /*
- Algo : Union-Find Algorithm
+ Algo : Disjoint set -Union By Rank and Path Compression
  * */
 
-public class DisjointSets {
+public class DisjointSetUnionAndPathCompression {
 
 	final int N;  // no of node
 	int[] parent;
-
-	public DisjointSets(int n) {
+	int[] rank;
+	
+	public DisjointSetUnionAndPathCompression(int n) {
 		super();
 		N = n;
 		this.parent = new int[n+1];
+		this.rank = new int[n+1];
 	}
 
 	int find(int n){
@@ -35,14 +37,23 @@ public class DisjointSets {
 	void unioin(int a, int b){
 		int root_a = this.find(a);
 		int root_b = this.find(b);
+
+		if(rank[root_a] < rank[root_b]){
+			this.parent[root_a] =root_b;
+		}else if(rank[root_b]< rank[root_a]){
+			this.parent[root_b]=root_a;
+		}else{
+			this.parent[root_b]=root_a;
+			this.rank[root_a]= this.rank[root_a]+1;
+		}
 		
-		this.parent[root_a]=root_b;
 		
 	}
 
 	void init(){
 		for(int i=1; i<= this.N; i++){
 			this.parent[i]=i;
+			this.rank[i]=0;
 		}
 	}
 
@@ -50,11 +61,12 @@ public class DisjointSets {
 
 		HashMap<Integer, Set<Integer>>  disSet =new HashMap<>();
 
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt(); // no of nodes
 		int M = sc.nextInt(); // no of pairs
 
-		DisjointSets ds = new DisjointSets(N);
+		DisjointSetUnionAndPathCompression ds = new DisjointSetUnionAndPathCompression(N);
 
 		ds.init();
 
